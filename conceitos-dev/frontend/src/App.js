6fb3063d14891265c3cd634ba4a969841eb0ac20
Marcello Costa <marcello88costa@yahoo.com.br>
2020-10-24 12:45:27 -0300

    First commit

---
+++ conceitos-dev/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header'
+
+function App() {
+    const projects = ['Desenvolvimento de app', 'frontend web'];
+
+    return (
+        <>
+            <Header title="Projects" />
+        </>
+    );
+}
+
+export default App;
